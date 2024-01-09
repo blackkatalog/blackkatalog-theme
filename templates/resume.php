b2f688937ddfdab67c3727f44fe82ff4db64f82e
blackkatalog <kattheodore77@gmail.com>
2024-01-08 22:29:30 -0500

    Init Commit

---
+++ templates/resume.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: resume */
+
+
+?>
+<div class="wrapper">
+    <div class="main container">
+        <div class="hero ">
+            <div class="row">
+                <div class="col-md-2 headshot">
+                    <img src="<?php get_site_url('/wp-content/uploads/2024/01/Group-3.png') ?>">
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+</div>
